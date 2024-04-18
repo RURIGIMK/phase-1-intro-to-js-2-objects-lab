361d2305aa2ce3769ca0fadd2d315558b1883c0c
RURIGIMK <rurigimk@gmail.com>
2024-04-18 22:07:46 +0300

    initial

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    const updatedEmployee = { ...employee };
+    updatedEmployee[key] = value;
+    return updatedEmployee;
+  };
+  
+  const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+  };
+  
+  const deleteFromEmployeeByKey = (employee, key) => {
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+  };
+  
+  const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+  };
+  
+  
+  
